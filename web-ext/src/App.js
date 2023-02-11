30c3e0b8b938a4cbde148770d58ebc699fd074b5
sid <sidwang2002@gmail.com>
2023-02-10 22:07:00 -0500

    basic set up

--- web-ext/src/App.js
+++ web-ext/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import  React, { useState , useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const DateTime = () => {
+
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+
+    return(
+        <div>
+            <p> Time : {date.toLocaleTimeString()}</p>
+            <p> Date : {date.toLocaleDateString()}</p>
+
+        </div>
+    )
 }
 
-export default App;
+export default DateTime
